0071de0c35ce76829b832a732fd9b5b3843813bb
Vishal-Asa <vishalasa16@gmail.com>
2024-11-25 12:12:22 +0530

    New Files

---
+++ src/main/java/SeJava/JavaApp/App.java
@@ -0,0 +1,13 @@
+package SeJava.JavaApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
